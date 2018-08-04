2277005c9c2c2f638ce9e167f805eaf84e5df354
Justin Brantley <justinbrantley@me.com>
2018-08-04 19:43:17 +0000

    Add scale/unscale functions

--- R/fit_models.R
+++ R/fit_models.R
@@ -6,16 +6,23 @@
 #' to predict brightness for the given features, and the other to predict color
 #' temperature.
 #'
+#' Note that quasibinomial models produce estimates in the range [0, 1], but
+#' brightness and color temperature are in the ranges [1, 254] and [153, 500],
+#' respectively. Scale/unscale functions are provided to help convert to and
+#' from the model scale.
+#'
 #' @param dta training dataset, like that returned by
 #'   \code{\link{training_data}}
+#' @param bri the brightness value to scale or unscale
+#' @param ct the color temperature value to scale or unscale
 #'
 #' @return Returns a named list with two models, one for brightness and one for
 #'   color temperature.
 #'
 #' @export
 fit_models <- function(dta = training_data()) {
-    dta$bri <- (dta$bri + 1) / (254 + 1)
-    dta$ct <- (dta$ct - 153) / (500 - 153)
+    dta$bri <- scale_bri(dta$bri)
+    dta$ct <- scale_ct(dta$ct)
 
     bri <- stats::glm(
         bri ~ room + role + group +
@@ -45,3 +52,19 @@ fit_models <- function(dta = training_data()) {
 
     return(list(bri = bri, ct = ct))
 }
+
+#' @rdname fit_models
+#' @export
+scale_bri <- function(bri) {(bri - 1) / (254 - 1)}
+
+#' @rdname fit_models
+#' @export
+unscale_bri <- function(bri) {bri * (254 - 1) + 1}
+
+#' @rdname fit_models
+#' @export
+scale_ct <- function(ct) {(ct - 153) / (500 - 153)}
+
+#' @rdname fit_models
+#' @export
+unscale_ct <- function(ct) {ct * (500 - 153) + 153}
