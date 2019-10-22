d03a99929d489a3e0bd55fff7957c86d6220400e
Justin Brantley <justinbrantley@me.com>
2019-10-22 04:25:33 +0000

    Fix "undefined global variable" error

--- R/models.R
+++ R/models.R
@@ -15,7 +15,7 @@
 #' @export
 fit_models <- function(dta = training_data()) {
     dta <- dplyr::group_by_at(dta, setdiff(names(dta), c('name', 'bri', 'ct', 'weight')))
-    dta <- dplyr::mutate(dta, weight = weight / dplyr::n())
+    dta$weight <- dta$weight / nrow(dta)
     dta <- dplyr::ungroup(dta)
 
     dta$bri <- scale_bri(dta$bri)
